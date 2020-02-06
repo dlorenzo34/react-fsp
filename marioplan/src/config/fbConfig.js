15a9c524ce999fa5a66734137f636dd6396f5167
Daniel Lorenzo <daniellorenzo@Daniels-MacBook-Pro.local>
2020-02-06 17:22:21 +0000

    first commit

---
+++ marioplan/src/config/fbConfig.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Replace this with your own config details
+var config = {
+  apiKey: "AIzaSyCi0MiQ7eh_UZi34bnagSCG7qDdUXstWm0",
+  authDomain: "notes-e-school.firebaseapp.com",
+  databaseURL: "https://notes-e-school.firebaseio.com",
+  projectId: "notes-e-school",
+  storageBucket: "notes-e-school.appspot.com",
+  messagingSenderId: "8610176078",
+  appId: "1:8610176078:web:59ffbbd3298f36f0f5771f",
+  measurementId: "G-V6NCDNCX47"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase 
+
